4a6d30fbb2d7110c3a3c69903b3eb190b919306b
Prabhu Preetam Das <77427356+urpreetam@users.noreply.github.com>
2022-04-20 13:49:00 +0530

    Create WeiredAlgorithm.cpp

---
+++ CSES_Problem_set/WeiredAlgorithm.cpp
@@ -0,0 +1,53 @@
+/* 
+
+Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+Your task is to simulate the execution of the algorithm for a given value of n.
+
+Input
+
+The only input line contains an integer n.
+
+Output
+
+Print a line that contains all values of n during the algorithm.
+
+Constraints
+1 <= n <= 10^6
+Example
+
+Input:
+3
+
+Output:
+3 10 5 16 8 4 2 1
+
+*/
+
+// Code // Code // Code // Code // Code
+
+#include<bits/stdc++.h>
+using namespace std;
+#define enout(n) cout<< n << "\n"
+#define sout(n)cout << n << " "
+#define endl "\n"
+typedef long long ll;
+
+int main(){
+    ll n;
+    cin >> n;
+    cout << n << " ";
+    while(n != 1){
+        if(n % 2 == 0){
+            n /= 2;
+            sout(n);
+        }
+        else{
+            n = n*3 + 1;
+            sout(n);
+            n/= 2;
+            sout(n);
+        }
+    }
+}
